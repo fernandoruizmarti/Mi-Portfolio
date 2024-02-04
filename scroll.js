a68b426f1036036184c4d6ad751e662a6de29df8
Nando Ruiz <158789651+fernandoruizmarti@users.noreply.github.com>
2024-02-05 00:07:48 +0100

    He añadido los archivos básicos de mi proyecto

---
+++ scroll.js
@@ -0,0 +1,32 @@
+/* --posicion inicial */
+let ubicacionPrincipal = window.pageYOffset;
+let $nav = document.querySelector("nav");
+
+/* --evento scroll */
+window.addEventListener("scroll", function() {
+    /* --muestra la ubicacion cada vez que hagas scroll */
+    //console.log(window.pageYOffset);
+
+    /* --donde nos encontramos actualmente */
+    let desplazamientoActual = window.pageYOffset;
+
+    /* --condicion para ocultar o mostrar el menu */
+    if(ubicacionPrincipal >= desplazamientoActual) {
+        /* --si es mayor o igual se muesta */
+        $nav.style.top = "0px";
+        console.log('Ubicacion Principal')
+        console.log(ubicacionPrincipal)
+        console.log('desplazamiento')
+        console.log(desplazamientoActual)
+    } else {
+        /* --sino lo ocultamos añadiendo un top negativo */
+        $nav.style.top = "-83px";
+        console.log('Ubicacion Principal')
+        console.log(ubicacionPrincipal)
+        console.log('desplazamiento')
+        console.log(desplazamientoActual)
+    }
+
+    /* --actulizamos la ubicacion principal */
+    ubicacionPrincipal = desplazamientoActual;
+});
